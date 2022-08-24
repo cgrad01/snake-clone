5135469873418c20a40b3b20cb68a77928258405
Corey Grad <Corey.Grad@Contractor.Prometric.com>
2022-08-24 11:51:20 -0400

    progress

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -10,35 +10,34 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
-        
         snake = this.GetComponent<Rigidbody2D>();
         snake.velocity = Vector3.right;
     }
     void Update()
     {
-        move();
+        Move();
     }
 
-     Vector3 lastDirection = Vector3.right;
-     private void move()
-     {
+    Vector3 lastDirection = Vector3.right;
+    private void Move()
+    {
         Vector3 direction = lastDirection;
-         if (Input.GetKeyDown(KeyCode.W) && Vector3.Angle(Vector3.up, lastDirection) == 90)
-         {
-             snake.velocity = Vector3.up;
-         }
-         if (Input.GetKeyDown(KeyCode.S) && Vector3.Angle(Vector3.down, lastDirection) == 90)
-         {
-             snake.velocity = Vector3.down;
-         }
-         if (Input.GetKeyDown(KeyCode.A))
-         {
-             snake.velocity = Vector3.left;
-         }
-         if (Input.GetKeyDown(KeyCode.D))
-         {
-             snake.velocity = Vector3.right;
-         }
-     }
+        if (Input.GetKeyDown(KeyCode.W) && Vector3.Angle(Vector3.up, lastDirection) == 90)
+        {
+            snake.velocity = Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && Vector3.Angle(Vector3.down, lastDirection) == 90)
+        {
+            snake.velocity = Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            snake.velocity = Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            snake.velocity = Vector3.right;
+        }
+    }
 
 }
